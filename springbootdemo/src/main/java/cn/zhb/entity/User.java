992aa6d0f6d10db1cba2eb54fa79e071905a567f
zhang <623904361@qq.com>
2021-06-03 17:24:17 +0800

    second

--- springbootdemo/src/main/java/cn/zhb/entity/User.java
+++ springbootdemo/src/main/java/cn/zhb/entity/User.java
@@ -8,4 +8,20 @@ package cn.zhb.entity;
 public class User {
     private Integer id;
     private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
