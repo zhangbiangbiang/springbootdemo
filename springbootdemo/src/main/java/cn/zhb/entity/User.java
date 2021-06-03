735174fa86142b100395c541ca633958d0263771
zhang <623904361@qq.com>
2021-06-03 17:23:24 +0800

    first

---
+++ springbootdemo/src/main/java/cn/zhb/entity/User.java
@@ -0,0 +1,11 @@
+package cn.zhb.entity;
+
+/**
+ * @author zhang
+ * @date 2021/6/3 17:20
+ */
+
+public class User {
+    private Integer id;
+    private String name;
+}
---
+++ springbootdemo/src/test/java/cn/zhb/SpringbootdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zhb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
